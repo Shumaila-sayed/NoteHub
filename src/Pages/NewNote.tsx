09de06a2e5bdd0eee2d9f70c353a077775a752f6
Shumaila <sayedshumaila27@gmail.com>
2025-07-10 00:54:08 +0530

    build out the form component

---
+++ src/Pages/NewNote.tsx
@@ -0,0 +1,14 @@
+import NoteForm from "../Components/NoteForm";
+
+const NewNote = () => {
+    return (
+        <>
+            <h1 className="mb-4">new note</h1>
+            <NoteForm/>
+        </>
+        
+    )
+
+}
+
+export default NewNote;
